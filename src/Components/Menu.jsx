5d83d0fbdda7d9cf2ae69da0cf89363f8ea94fb5
MacBookAir <macbookair@MacBookAirs-MacBook-Air.local>
2022-10-14 17:05:47 +0400

    Add: Added styled components and antd, created a styled file for header

---
+++ src/Components/Menu.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Head from "../Styled/Header.styled";
+
+export default function Menu() {
+  return <Head>Menu</Head>;
+}
---
+++ src/Styled/Header.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Head = styled.div`
+font-size: 16px;
+line-height: 19.78px;
+line-height: 1%;
+color: #000000;
+display: flex;
+flex-direction: row;
+background-color: red
+`
+export default Head
